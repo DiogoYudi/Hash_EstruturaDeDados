5347b6d0ab560c47c89f89a4ab026bdce3c593c3
DiogoYudi <130068159+DiogoYudi@users.noreply.github.com>
2023-04-20 17:15:18 -0300

    Update EX28.cs

--- EX28.cs
+++ EX28.cs
@@ -2,7 +2,8 @@
 //	Dentro de cada opção deve haver as funcionalidades: Inserir, Alterar e Relatar.
 //	O vetor deve ser do tipo abstrato de dado composto por idade, nome e whats. Serão necessários 3 vetores, um para cada tipo de tratamento de colisão.
 //Para inserir um novo registro, solicite a idade, nome e whats. Utilize a idade como chave.
-//Para alterar solicite a idade (chave) para ser utilizada na busca. Caso encontrada, informe o nome e o whats da pessoa. Após a consulta, o usuário pode atualizar somente o nome e o whats.
+//Para alterar solicite a idade (chave) para ser utilizada na busca. Caso encontrada, informe o nome e o whats da pessoa. 
+//Após a consulta, o usuário pode atualizar somente o nome e o whats.
 //Para relatar, percorra o vetor do inicio ao fim e exiba todos os registros.
 
 
